a7ede74188a7f7df2aa1d3d270b7afb1dd6c16cd
KarlLundengaard <106549720+KarlLundengaard@users.noreply.github.com>
2022-07-19 19:48:44 +0000

    Modify imports in template so that evaluation_tests.py can be run from inside the 'app' folder
    The import code now checks if evaluation.py is in the parent folder and if it is not it checks if it is in the current folder.

--- app/evaluation_tests.py
+++ app/evaluation_tests.py
@@ -1,6 +1,9 @@
 import unittest
 
-from .evaluation import evaluation_function
+try:
+    from .evaluation import evaluation_function
+except ImportError:
+    from evaluation import evaluation_function
 
 class TestEvaluationFunction(unittest.TestCase):
     """
